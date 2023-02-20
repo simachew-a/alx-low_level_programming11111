1e8a18908f77911d0fca05132ee47d2005bc86dd
simachew-a <smith562258@gmail.com>
2023-02-19 22:49:12 -0800

    ghp_u0waZLe3JmV5lP43WwTu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-        printf("size of a chare :%i byte(s)\n", sizeof(char));
+	printf("size of a chare :%i byte(s)\n", sizeof(char));
 	printf("size of an int :%i byte(s)\n", sizeof(int));
 	printf("size of a long int :%i byte(s)\n", sizeof(long int));
-	printf("size of a lon long int :%i byte(s)\n", sizeof(long long int ));
-	printf("size of a float :%i byte(s)\n", sizeof(float));
+	printf("size of a lon long int :%i byte(s)\n", sizeof(long long int));
+	printf("size of a lon long int :%i byte(s)\n", sizeof(long long int));
 	return (0);
 }
